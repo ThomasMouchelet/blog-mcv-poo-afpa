9396ea9e390b415db6f5b9e5cdde1f9fc8888da9
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2021-11-08 15:26:46 +0100

    blog

--- public/index.php
+++ public/index.php
@@ -5,11 +5,14 @@ require_once "../config/Autoloader.php";
 use App\config\Autoloader;
 Autoloader::register();
 
-use App\src\Controller\HomeController;
+use App\src\Controller\ArticleController;
 
 if(isset($_GET['route'])){
     // rediriger vers une page
+    if($_GET['route'] === "single"){
+
+    }
 }else{
-    $appController = new HomeController();
+    $appController = new ArticleController();
     $appController->home();
 }
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,11 +4,16 @@ namespace App\src\Controller;
 
 use App\src\Repository\ArticleRepository;
 
-class HomeController
+class ArticleController
 {
     public function home(){
         $repoArticle = new ArticleRepository();
         $articles = $repoArticle->getArticles();
         require "../templates/home.php";
+    }
+    public function single(){
+        // get repository
+        // getArticle()
+        // require single.php
     }
 }
