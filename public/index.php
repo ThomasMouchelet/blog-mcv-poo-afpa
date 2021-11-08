f6d2c51f119cd55cc0d10c1c8cfbb6f8ca7e0bda
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2021-11-08 11:15:24 +0100

    blog

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+// ?route=home
+if(isset($_GET['route'])){
+    // rediriger vers une page
+}else{
+    require "../templates/home.php";
+}
---
+++ templates/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div>
+        <h1>Title 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi cum distinctio voluptate sunt facere id libero dignissimos! Architecto, expedita! Nihil iure fugiat vel illum esse ut sint laborum quos quae.</p>
+        <a href="">Lire l'article</a>
+    </div>
+    <div>
+        <h1>Title 2</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi cum distinctio voluptate sunt facere id libero dignissimos! Architecto, expedita! Nihil iure fugiat vel illum esse ut sint laborum quos quae.</p>
+        <a href="">Lire l'article</a>
+    </div>
+
+</body>
+</html>
